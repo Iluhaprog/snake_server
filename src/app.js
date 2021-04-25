3edfd1485e13f65576aabc2ab857c67f3c0d86e7
Ilya Novak <rickmortyand4@gmail.com>
2021-04-25 12:51:00 +0300

    Added logic

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const scores = [
+    {
+        title: 'Ilya',
+        score: 100,
+    },
+];
+
+app.use(cors({
+    origin: 'http://localhost:5500',
+    optionsSuccessStatus: 200,
+}));
+
+app.get('/scores', (req, res) => {
+    const result = scores.map((item) => `
+        <p><span>${item.title}</span><span>${item.score}</span></p>
+    `);
+    res.send(result);
+});
+
+app.post('/setScore', (req, res) => {
+    const {nik: title, score} = req.query;
+    scores.unshift({title, score});
+    res.send('ok');
+});
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
